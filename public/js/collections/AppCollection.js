8f799dc23448acb36d77267e8ca352291a9c7c35
Edward Yang <edward.yang@magnet.com>
2015-01-21 16:46:39 -0800

    validate case insensitive for app name

--- public/js/collections/AppCollection.js
+++ public/js/collections/AppCollection.js
@@ -4,6 +4,11 @@ define(["jquery", "backbone", "models/AppModel"], function($, Backbone, AppModel
         urlRoot: 'apps',
         parse: function(res){
             return res.data;
+        },
+        iwhere: function(key, val){
+            return this.filter(function(item){
+                return item.get(key).toLowerCase() === val.toLowerCase();
+            });
         }
     });
     return Collection;
